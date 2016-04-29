d4efc4b898d660f17686234d0eadb146ec553f81
Joel Barna <joel.tbarna@gmail.com>
2016-04-29 18:23:54 -0400

    really hoping everything is fixed

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 
 var gui = require('./lib/gui_controller'),
     crawlManager = require('./lib/crawl_manager.js'),
-    //startupManager = require('./lib/startup_manager'),
+    startupManager = require('./lib/startup_manager'),
     processListeners = require('./lib/process_listeners');
 
 
 processListeners();
-
 startupManager();
 gui.create();
-setTimeout( () => gui.newMessage("Testing", "www.google.com"));
 crawlManager();
