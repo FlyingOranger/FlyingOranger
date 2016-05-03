4fd9e83f461a3ef057fb4a66247f5c418f576863
Joel Barna <joel.tbarna@gmail.com>
2016-05-03 13:48:07 -0400

    Changed crawlers to notifacation_apps

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 "use strict"
 
 var gui = require('./lib/gui_controller'),
-    crawlManager = require('./lib/crawl_manager.js'),
+    appManager = require('./lib/apps_manager.js'),
     processListeners = require('./lib/process_listeners');
 
 
 processListeners();
 gui.create();
-crawlManager();
+appManager();
